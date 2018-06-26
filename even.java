cd8d71b55823b823a68448f6260462083c92d963
divyasundharam <divyasundharam8@gmail.com>
2018-06-26 22:38:37 +0530

    Update even.java

--- even.java
+++ even.java
@@ -6,6 +6,8 @@ public static void main(String args[])
 {
 Scanner sc=new Scanner(System.in);
 int a=sc.nextInt();
+  if(a>)
+  {
 if(a%2==0)
 {
 System.out.print("even");
@@ -14,5 +16,10 @@ else
 {
 System.out.print("odd");
 }
+  }
+  else
+  {
+    System.out.print("invalid");
+  }
 }
 }
